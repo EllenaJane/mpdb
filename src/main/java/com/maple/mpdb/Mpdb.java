09e8c707e726d9f2e83322ae589ac0f068679db8
Khaing Lay Mon <khaing.lay.mon@facilis.aero>
2020-01-14 21:26:28 +0630

    Add Main class

---
+++ src/main/java/com/maple/mpdb/Mpdb.java
@@ -0,0 +1,16 @@
+package com.maple.mpdb;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Khaing Lay Mon
+ *
+ */
+@Component
+@ComponentScan
+public class Mpdb {
+
+  public static final String VERSION = "0.0.1";
+
+}
