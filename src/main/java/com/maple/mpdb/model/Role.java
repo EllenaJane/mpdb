f9f53ebba5fb97092ba4ce0e771893fc3d402716
Khaing Lay Mon <khaing.lay.mon@facilis.aero>
2020-01-23 23:09:10 +0630

    Add User, Role and Organization model and JPA repository

---
+++ src/main/java/com/maple/mpdb/model/Role.java
@@ -0,0 +1,19 @@
+package com.maple.mpdb.model;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Role implements Serializable {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private int id;
+  private String name;
+
+}
