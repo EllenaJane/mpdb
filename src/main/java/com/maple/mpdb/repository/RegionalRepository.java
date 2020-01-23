db747b45db155b48cec40ce23dddcc5109dcd996
Yee Mon Phu <yee.mon.phu@facilis.aero>
2020-01-23 22:48:45 +0630

    Modify regional repository

--- src/main/java/com/maple/mpdb/repository/RegionalRepository.java
+++ src/main/java/com/maple/mpdb/repository/RegionalRepository.java
@@ -2,9 +2,15 @@ package com.maple.mpdb.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import com.maple.mpdb.model.Regional;
+import com.maple.mpdb.model.Region;
 
+/**
+ * Repository layer for managing regional transactions
+ *
+ * @author phulay
+ *
+ */
 @Repository
-public interface RegionalRepository extends JpaRepository<Regional, Integer>{
+public interface RegionalRepository extends JpaRepository<Region, Integer>{
 	
 }
