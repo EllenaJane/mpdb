0ab775e67ef2dfa24ce3b9d785deaed3c3cb39c0
Khaing Lay Mon <khaing.lay.mon@facilis.aero>
2020-02-01 10:44:23 +0630

    Implement findByType and findByMedia

--- src/main/java/com/maple/mpdb/repository/JournalRepository.java
+++ src/main/java/com/maple/mpdb/repository/JournalRepository.java
@@ -1,9 +1,11 @@
 package com.maple.mpdb.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.maple.mpdb.model.Journal;
+import com.maple.mpdb.model.Media;
+import com.maple.mpdb.model.Type;
 
 /**
  * Repository layer for managing journal transactions
@@ -13,5 +15,9 @@ import com.maple.mpdb.model.Journal;
  */
 @Repository
 public interface JournalRepository extends JpaRepository<Journal, Integer> {
+
+  List<Journal> findByType(final Type type);
+
+  List<Journal> findByMedia(final Media media);
 
 }
