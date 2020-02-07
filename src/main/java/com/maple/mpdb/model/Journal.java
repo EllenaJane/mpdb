81348655e1debd21250de45b67435197d872f6a9
Yee Mon Phu <yee.mon.phu@facilis.aero>
2020-02-07 20:24:31 +0630

    Declare annotation to change enum to string

--- src/main/java/com/maple/mpdb/model/Journal.java
+++ src/main/java/com/maple/mpdb/model/Journal.java
@@ -3,6 +3,8 @@ package com.maple.mpdb.model;
 import java.io.Serializable;
 import java.time.Instant;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -16,6 +18,7 @@ public class Journal implements Serializable {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Integer id;
+  @Enumerated(EnumType.STRING)
   private Type type;
   private Instant publishedDate;
 
