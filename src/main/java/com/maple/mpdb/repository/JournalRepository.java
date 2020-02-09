683071e65a498bf3552d203fb106dba521fe4f28
Khaing Lay Mon <khaing.lay.mon@facilis.aero>
2020-02-10 00:13:24 +0630

    Search type using string instead of enum default value int

--- src/main/java/com/maple/mpdb/repository/JournalRepository.java
+++ src/main/java/com/maple/mpdb/repository/JournalRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import com.maple.mpdb.model.Journal;
-import com.maple.mpdb.model.Type;
 
 /**
  * Repository layer for managing journal transactions
@@ -19,6 +18,6 @@ public interface JournalRepository extends JpaRepository<Journal, Integer> {
 
   @Query("SELECT j FROM Journal j WHERE (?1 is null or j.type = ?1) and  (?2 is null or j.media.id = ?2) and "
       + "(?3 is null or j.publishedDate >= ?3) and (?4 is null or j.publishedDate <= ?4)")
-  List<Journal> search(final Type type, final Integer mediaId, final Date fromDate,
+  List<Journal> search(final String type, final Integer mediaId, final Date fromDate,
       final Date toDate);
 }
