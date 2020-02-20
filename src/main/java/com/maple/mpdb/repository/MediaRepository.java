f42748597648cf83e394bfacdc7454739095e2cc
Yee Mon Phu <yee.mon.phu@facilis.aero>
2020-02-20 11:42:17 +0630

    Adapt some implementation

--- src/main/java/com/maple/mpdb/repository/MediaRepository.java
+++ src/main/java/com/maple/mpdb/repository/MediaRepository.java
@@ -2,7 +2,6 @@ package com.maple.mpdb.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.maple.mpdb.model.Media;
 
 /**
@@ -14,4 +13,8 @@ import com.maple.mpdb.model.Media;
 @Repository
 public interface MediaRepository extends JpaRepository<Media, Integer> {
 
+  /*
+   * @Query("SELECT m FROM Media m WHERE m.region.id = :regionId") List<Media>
+   * getMediaListByRegion(@Param("regionId") Integer regionId);
+   */
 }
